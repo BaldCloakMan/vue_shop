e4833ec02d5b1cdcf8df33b6e6ee18447fa9cfcf
BaldCloakMan <540002338@qq.com>
2020-12-14 19:16:49 +0800

    登录组件完成

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-row>
+      <el-button type="info" @click="loginout">退出</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    loginout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+
+<style>
+</style>
